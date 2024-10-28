18cd6fc207d663989c0c6c64725895d4dbdf7721
EpheyHertz <151906589+EpheyHertz@users.noreply.github.com>
2024-10-28 16:56:11 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,33 +3,35 @@ import { NextResponse } from "next/server";
 // Define the matcher configuration for middleware
 export const config = {
     matcher: [
-        // Protected routes
         '/profile/:path*',
         '/updateprofile/:path*',
         '/latestnews/:path*',
         '/talktous/:path*',
         '/chatai/:path*',
-        // Public routes
         '/auth/login',
         '/auth/signup',
-        'auth/password-reset',
+        '/auth/password-reset',
         '/'
     ],
 };
 
 export function middleware(request) {
     const { pathname } = request.nextUrl;
-    
+
     // Access the access token from cookies
     const accessToken = request.cookies.get('accessToken');
-   
+
+    // Log values for debugging
+    console.log("Pathname:", pathname);
+    console.log("Access Token:", accessToken);
+
     if (accessToken) {
         // If authenticated, redirect away from public routes
         if (pathname.startsWith('/auth') || pathname === '/') {
             return NextResponse.redirect(new URL('/profile', request.url));
         }
     } else {
-        
+        // If not authenticated, redirect protected routes to login
         if (pathname.startsWith('/profile') || pathname.startsWith('/updateprofile') || 
             pathname.startsWith('/latestnews') || pathname.startsWith('/talktous') || 
             pathname.startsWith('/chatai')) {
@@ -37,6 +39,5 @@ export function middleware(request) {
         }
     }
 
-    
     return NextResponse.next();
 }
