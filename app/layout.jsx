2eb345669d7f03715873a495542c7e8fe336fb95
EpheyHertz <mwain579@gmail.com>
2025-01-17 11:22:03 +0300

    added google meta data

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
+      <meta name="google-site-verification" content="IgPxvlBVBb5z9Qvfgv34KXZPyVNGQXGtI_XHOS7VMSo" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
